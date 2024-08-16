b7bc6c73fc9dadaa7ab8736a70e3835e92b242ae
Daniel Kowalski <77574206+k0wal45@users.noreply.github.com>
2024-08-16 13:57:15 +0200

    added error if none of the components exist under slug

--- app/components/[slug]/page.tsx
+++ app/components/[slug]/page.tsx
@@ -2,6 +2,7 @@
 import ComponentShowcase from "@/components/ComponentShowcase/ComponentShowcase";
 import componentsData from "@/DATA/data";
 import { filterComponentsBySlug } from "@/DATA/helpers";
+import Link from "next/link";
 import { useParams, usePathname } from "next/navigation";
 import React from "react";
 
@@ -10,15 +11,42 @@ const page = () => {
 
   const data = filterComponentsBySlug(componentsData, slug);
 
-  return (
-    <div className="mt-24 flex flex-col gap-8 p-4">
-      <div className="flex flex-col">
-        <p className="text-neutral-600">Components / {data.name}</p>
-        <h1 className="text-4xl text-neutral-900 font-semibold">{data.name}</h1>
+  if (data.components.length !== 0) {
+    return (
+      <div className="mt-24 flex flex-col gap-8 p-4">
+        <div className="flex flex-col">
+          <p className="text-neutral-600">Components / {data.name}</p>
+          <h1 className="text-4xl text-neutral-900 font-semibold">
+            {data.name}
+          </h1>
+        </div>
+        {data.components.map((component) => (
+          <ComponentShowcase component={component} key={component.name} />
+        ))}
       </div>
-      {data.components.map((component) => (
-        <ComponentShowcase component={component} key={component.name} />
-      ))}
+    );
+  }
+
+  return (
+    <div className="w-full h-screen flex flex-col gap-6 items-center justify-center text-center">
+      <h1 className="text-5xl lg:text-7xl font-bold">Opppps!</h1>
+      <p className="text-xl max-w-xl">
+        Something went wrong, no components here :/
+      </p>
+      <div className="flex flex-wrap gap-4 items-center justify-center">
+        <Link
+          href=""
+          className="p-4 px-6 bg-black text-white font-bold text-lg hover:bg-neutral-800 active:scale-105 transition-all duration-200"
+        >
+          Home
+        </Link>
+        <Link
+          href=""
+          className="p-4 px-6 text-black border-4 scale-95 border-black font-bold text-lg hover:bg-neutral-800/10 active:scale-105 transition-all duration-200"
+        >
+          Browse Components
+        </Link>
+      </div>
     </div>
   );
 };

10e4ae79958cebcd9fa5f8a850ee418d4575b575
Daniel Kowalski <77574206+k0wal45@users.noreply.github.com>
2024-08-28 13:32:22 +0200

    code fix

--- other/Cards/ShuffleInBack/ShuffleInBackShowOf.tsx
+++ other/Cards/ShuffleInBack/ShuffleInBackShowOf.tsx
@@ -9,28 +9,34 @@ export const ShuffleInBackShowOf = () => {
   );
 };
 
-export const ShuffleInBackCode = `import { FaPerson } from "react-icons/fa6";
+export const ShuffleInBackCode = `import Image from "next/image";
+import React from "react";
 
-const DarkOnHover = () => {
+const ShuffleInBack = () => {
   return (
-    <div className="relative w-72 overflow-hidden flex flex-col justify-between gap-4 p-12 bg-white shadow-sm text-start shrink-0 cursor-pointer transition-all hover:scale-[1.015] hover:shadow-xl group">
-      <div className="flex justify-between items-center py-4 border-b-2 border-neutral-300">
-        <FaPerson className="text-5xl text-orange-500" />
-        <p className="text-neutral-400 font-bold text-xl group-hover:text-white  transition-all duration-700">
-          01
-        </p>
-      </div>
-      <h4 className="text-2xl group-hover:text-neutral-100  transition-all duration-700">
-        Hire us
-      </h4>
-      <p className="text-neutral-600  group-hover:text-neutral-300 transition-all duration-700">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora modi
-        cumque possimus totam id laboriosam qui nihil ipsam aliquam harum.
+    <div
+      className="
+      aspect-square bg-white rounded-[3.2rem] relative p-8 max-w-72 flex flex-col items-center gap-4 z-20 shadow-lg duration-200 hover:scale-105
+      after:absolute after:size-full after:top-0 after:left-0 after:bg-white/40 after:rounded-[3.2rem] after:z-[-2] after:origin-bottom-left after:rotate-[4deg] after:translate-y-[-1rem] after:duration-200 hover:after:rotate-[10deg] hover:after:translate-y-[-3rem] hover:after:translate-x-4 hover:after:scale-95
+      before:absolute before:size-full before:top-0 before:left-0 before:bg-white/40 before:rounded-[3.2rem] before:z-[-2] before:origin-bottom-left before:rotate-[8deg] before:translate-y-[-2rem] before:duration-200 hover:before:rotate-[20deg] hover:before:translate-y-[-5.1rem] hover:before:translate-x-4 hover:before:scale-95
+
+  "
+    >
+      <Image
+        width={100}
+        height={100}
+        alt="Person profile"
+        src="https://images.unsplash.com/photo-1543610892-0b1f7e6d8ac1?q=80&w=1856&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        className="w-24 aspect-square rounded-xl object-cover"
+      />
+      <p className="text-xl text-neutral-800 text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
+        necessitatibus magni ipsam ipsum sed earum officia incidunt porro totam
+        eos.
       </p>
-      <div className="size-full absolute top-0 left-0 bg-neutral-900 z-[-2] -translate-x-full group-hover:translate-x-0 duration-300 transition-transform"></div>
     </div>
   );
 };
 
-export default DarkOnHover;
+export default ShuffleInBack;
 `;

2ca8a391059fa7de3b9cbdadc3fec91dfbcfd3ed
Daniel Kowalski <77574206+k0wal45@users.noreply.github.com>
2024-08-28 11:07:22 +0200

    google analitics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const font = Montserrat({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         {children}
         <Footer />
       </body>
+      <GoogleAnalytics gaId="G-MWHD6R8C9L" />
     </html>
   );
 }

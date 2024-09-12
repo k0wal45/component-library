45fb99d6ed7282758a0e541b75342daedc15932e
Daniel Kowalski <77574206+k0wal45@users.noreply.github.com>
2024-09-12 16:59:26 +0200

    new page, templates coming soon

---
+++ app/templates/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <main className="grid place-items-center mx-auto h-[80vh]">
+      <h1 className="text-8xl font-bold text-neutral-800">In development...</h1>
+    </main>
+  );
+};
+
+export default page;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -30,6 +30,20 @@ const Navbar = () => {
               style={{ clipPath: "polygon(10% 0, 90% 0, 100% 100%, 0% 100%)" }}
             ></div>
           </li>
+          <li className="relative h-full flex items-center">
+            <Link href="/templates" className="mx-4">
+              Templates
+            </Link>
+
+            <div
+              className={`absolute w-full bottom-0 left-0 h-2 bg-primary z-[-1] transition-transform duration-600 ease-in-out ${
+                curretPath.startsWith("/templates")
+                  ? "translate-y-0"
+                  : "translate-y-12"
+              }`}
+              style={{ clipPath: "polygon(10% 0, 90% 0, 100% 100%, 0% 100%)" }}
+            ></div>
+          </li>
         </ul>
       </div>
     </nav>

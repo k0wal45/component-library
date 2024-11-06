2adda6442279cff4e28a26a363e06ded8eae2845
Daniel Kowalski <77574206+k0wal45@users.noreply.github.com>
2024-11-06 16:59:24 +0100

    Update Hero.tsx

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -25,17 +25,7 @@ const Hero = () => {
           </p>
         </Reveal>
         <Reveal>
-          <div className="flex flex-wrap gap-4">
-            <GradientButton link="/components">Components</GradientButton>
-            <Link
-              href="https://daniel-kowalski.com/"
-              className="p-4 px-8 bg-transparent text-center border-neutral-900 border-2 rounded-md text-neutral-900 font-bold grid place-items-center text-xl transition-all duration-300
-            hover:bg-neutral-900/5 active:translate-x-[4px] active:translate-y-[4px] active:shadow-[-2px_-2px_0px_rgb(23,23,23)] 
-      "
-            >
-              Hire Me
-            </Link>
-          </div>
+            <GradientButton link="/components">Components</GradientButton>  
         </Reveal>
       </div>
       <AppearFromBottom>
